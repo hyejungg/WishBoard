30bae69f2806dd539c689ef45e1f789b1c9a91ec
youngjinc <cyjin6789@gmail.com>
2021-05-11 23:15:57 +0900

    Feature:#28

---
+++ app/src/main/java/com/hyeeyoung/wishboard/adapter/NotiAdapter.java
@@ -0,0 +1,63 @@
+package com.hyeeyoung.wishboard.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hyeeyoung.wishboard.R;
+import com.hyeeyoung.wishboard.item.NotiItem;
+import com.hyeeyoung.wishboard.item.WishItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotiAdapter extends RecyclerView.Adapter<NotiAdapter.CustomViewHolder> {
+    private ArrayList<NotiItem> notiList;
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        protected ImageView item_image;
+        protected TextView item_name;
+        protected TextView noti_type;
+        protected TextView noti_date;
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.item_image = (ImageView) view.findViewById(R.id.item_image);
+            this.item_name = (TextView) view.findViewById(R.id.item_name);
+            this.noti_type = (TextView) view.findViewById(R.id.noti_type);
+            this.noti_date = (TextView) view.findViewById(R.id.noti_date);
+        }
+    }
+    public NotiAdapter(ArrayList<NotiItem> data) {
+        this.notiList = data;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.noti_ltem, viewGroup, false);
+
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
+        NotiItem item = notiList.get(position);
+        viewholder.item_image.setImageResource(item.getItem_image());
+        viewholder.item_name.setText(item.getItem_name());
+        viewholder.noti_type.setText(item.getNoti_type());
+        viewholder.noti_date.setText(item.getNoti_date());
+    }
+    @Override
+    public int getItemCount() {
+        return (null != notiList ? notiList.size() : 0);
+    }
+}
