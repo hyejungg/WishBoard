4f6c507874da8cdc00b4ed42d958904fa73427bb
hyejung <kimhyejung12@naver.com>
2021-05-30 23:59:41 +0900

    Mod:#59

---
+++ app/src/main/java/com/hyeeyoung/wishboard/sign/KakaoApplication.java
@@ -0,0 +1,22 @@
+package com.hyeeyoung.wishboard.sign;
+
+import android.app.Application;
+
+import com.hyeeyoung.wishboard.R;
+import com.kakao.sdk.common.KakaoSdk;
+
+/**
+ * KakaoDeveloper 사이트에서 초기화해주는 과정을 별도의 Application class 파일로 빼두길 권고
+ * Kakao Login API version 2("com.kakao.sdk:v2-user:2.2.0") Java 8 이상 권고
+ * KakaoDeveloper 사이트에서는 Kotiln을 기반으로 작성되어 있음. 콜백메소드 역시 Kotiln으로 구성
+ * */
+
+public class KakaoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // @brief : kakao 네이티브 앱키로 초기화
+        String kakao_app_key = getResources().getString(R.string.kakao_app_key);
+        KakaoSdk.init(this, kakao_app_key);
+    }
+}
