089731de99943a524f470ca1ea4d8e3b5c30046b
youngjinc <cyjin6789@gmail.com>
2021-06-02 15:52:41 +0900

    Feature:#84

---
+++ app/src/main/java/com/hyeeyoung/wishboard/remote/IRemoteService.java
@@ -0,0 +1,42 @@
+package com.hyeeyoung.wishboard.remote;
+
+import com.hyeeyoung.wishboard.model.WishItem;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface IRemoteService {
+
+    //@brief : 네트워크 설정
+    String BASE_URL = "http://13.125.227.20:3000";
+
+    /*@brief : 각 요청 URL
+    String NEW_ITEM_URL = BASE_URL+"/item/";
+    String HOME_URL = BASE_URL+"/home/";
+    */
+
+    /**
+     * @brief NewItemFragment : 서버에 아이템 등록
+     */
+
+    /*
+    @brief : POST 방식, BASE_URL/item 호출
+     */
+
+    @POST("/item/new")
+    Call<ResponseBody> insertItemInfo(@Body WishItem wish_item);
+
+    /*
+    @brief : GET 방식, BASE_URL/home/{user_id} 호출
+    Data Type의 JSON을 통신을 통해 받음
+    @Path("user_id") String id : id 로 들어간 String 값을 {user_id}에 넘김
+    @return WishItem 객체를 JSON 형태로 반환
+     */
+
+    @GET("/home/{user_id}")
+    Call<WishItem> getData(@Path("user_id") String user_id);
+}
---
+++ app/src/main/java/com/hyeeyoung/wishboard/remote/ServiceGenerator.java
@@ -0,0 +1,20 @@
+package com.hyeeyoung.wishboard.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public interface ServiceGenerator {
+    /**
+     * @brief : 원격 호출을 정의한 인터페이스 메소드를 호출할 수 있는 서비스 생성
+     */
+    public static <S> S createService(Class<S> serviceClass){
+
+        // @brief : 여기서 레트로핏의 핵심 생성
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(IRemoteService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+//                .client(httpClient.build())
+                .build();
+        return retrofit.create(serviceClass);
+    }
+}
