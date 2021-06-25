3da0b9f2896f4a264921d2d5504c1888355b958e
youngjinc <cyjin6789@gmail.com>
2021-06-25 21:03:37 +0900

    Mod:#142

--- node_server/routes/basket.js
+++ node_server/routes/basket.js
@@ -7,7 +7,7 @@ router.get("/:user_id", function (req, res) {
   var user_id = Number(req.params.user_id);
 
   var sql =
-  "SELECT a.item_image, a.item_name, a.item_price, b.item_count FROM items a JOIN basket b ON a.item_id = b.item_id WHERE b.user_id = ? ORDER BY a.item_id DESC";
+  "SELECT a.item_id, a.item_image, a.item_name, a.item_price, b.item_count FROM items a JOIN basket b ON a.item_id = b.item_id WHERE b.user_id = ? ORDER BY a.item_id DESC";
 
   console.log("sql_select : " + sql);
 
@@ -78,7 +78,6 @@ router.delete("/", function (req, res) {
   var item_id = Number(req.body.item_id);
 
   console.log(user_id + " " + item_id);
-	
 
   var sql = "DELETE FROM basket WHERE user_id = ? AND item_id = ?";
   var params = [user_id, item_id];
@@ -107,5 +106,106 @@ router.delete("/", function (req, res) {
   });
 });
 
-module.exports = router;
+// @brief : 장바구니 수정
+router.put("/", function (req, res) {
+   console.log(req.body);
+	console.log(req.body.length);
+  // var user_id = Number(req.params.user_id);
+  // var item_id = Number(req.params.item_id);
 
+  // console.log(user_id + " " + item_id);
+
+  // var sql =
+  //   "UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?";
+
+  var sqls = "";
+ /* var values = [
+     { user_id : Number(req.body.user_id),
+       item_id : Number(req.body.item_id),
+       item_count : Number(req.body.item_count)}
+   ];*/
+
+  // values.forEach(function(item){
+  //   sqls += mysql.format("UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?", item));
+  // })
+
+  //for (var item in values) {
+  //  sqls += db.format(
+  //    "UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?",
+  //    item
+  //  );
+  //}
+	//
+  // var params = [user_id, item_id];
+
+   for(var i = 0; i < req.body.length; i++){
+  //  console.log(values[i] + "\n");
+    
+    console.log(req.body[i].user_id + " " + req.body[i].item_id + " " + req.body[i].item_count + " " );
+  }
+
+/*  values.forEach(function (item){
+    sqls += "UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?"
+      ,item.item_count,
+      item.user_id,
+      item.item_id;
+  });
+
+	console.log(sqls);
+
+  db.get().query(sqls, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length === 0) {
+        console.log("Failed to deleted the basket for data.");
+        res.status(500).json({
+          success: false,
+          message: "wish boarad 서버 에러",
+        });
+      } else {
+        console.log("Successfully deleted data into the basket!!");
+        res.status(200).json({
+          success: true,
+          message: "장바구니 데이터베이스 수정 성공",
+          item_count: result[0].item_count,
+          item_price: result[0].item.item_price,
+        });
+      }
+      db.releaseConn();
+    }
+  }); */
+
+	sqls = "UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?"
+    
+    for(var i = 0; i < req.body.length; i++){
+	//sqls = "UPDATE basket SET item_count = ? WHERE user_id = ? AND item_id = ?"
+    console.log(sqls);
+    
+    console.log(req.body[i].item_count, req.body[i].user_id, req.body[i].item_id);
+		db.get().query(sqls,[req.body[i].item_count, req.body[i].user_id, req.body[i].item_id], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (result.length === 0) {
+          console.log("Failed to updated the basket for data.");
+          res.status(500).json({
+            success: false,
+            message: "wish boarad 서버 에러",
+          });
+        } else {
+	  if(i === req.body.length){	
+            console.log("Successfully updated data into the basket!!");
+  	    res.status(200).json({
+              success: true,
+              message: "장바구니 데이터베이스 수정 성공",
+            });
+         }
+	}
+        db.releaseConn();
+      }
+    });
+  }
+});
+
+module.exports = router;
