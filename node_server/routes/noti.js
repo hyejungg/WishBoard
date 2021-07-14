e1f9ce8679707b2aaecd5f4a524765fcecdaa007
youngjinc <cyjin6789@gmail.com>
2021-07-15 01:36:12 +0900

    Feature:#156

--- node_server/routes/noti.js
+++ node_server/routes/noti.js
@@ -2,8 +2,7 @@ var express = require("express");
 var db = require("../db");
 var router = express.Router(); // @brief : express.Router() : router 객체를 생성
 
-// @brief '/' : 함수가 적용되는 경로(라우트)
-// /noti
+// @brief : 알림 등록
 router.post("/", function(req, res) {
     var user_id = Number(req.body.user_id);
     var item_id = Number(req.body.item_id);
@@ -37,4 +36,63 @@ router.post("/", function(req, res) {
     });
 });
 
+// @brief : 알림 조회
+router.get('/:user_id', function(req, res, next) {
+    var user_id = req.params.user_id;
+    console.log("user_id : " + user_id);
+    
+    var sql = "SELECT i.item_id, i.item_image, item_name, item_notification_type, CAST(item_notification_date AS CHAR(19)) item_notification_date, is_read FROM notification n JOIN items i ON n.item_id = i.item_id WHERE n.user_id = ? and item_notification_date <= NOW() ORDER BY item_notification_date DESC";
+
+    console.log("sql : " + sql);
+
+    db.get().query(sql, [user_id], function (err, rows) {
+          if (err) {
+               console.log(err);
+               res.sendStatus(400);
+             } else {
+               if (rows.length === 0) {
+                 console.log("Failed to select notification data.");
+                 res.status(500).json({
+                   success: false,
+                   message: "wish boarad 서버 에러",
+                 });
+               } else {
+                 console.log("rows : " + JSON.stringify(rows));
+		 res.status(200).json(rows);
+               }
+             }
+         db.releaseConn();
+    });
+});
+
+// @brief : 사용자가 조회한 알림은 읽은 알림으로 처리하기
+router.put('/:user_id', function(req, res){
+  var user_id = Number(req.params.user_id);
+  console.log("user_id : " + user_id)
+
+  var sql = "UPDATE notification SET is_read = 1 WHERE user_id = ? AND is_read = 0";
+  console.log("sql_update : " + sql);
+
+  db.get().query(sql, [user_id], function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length === 0) {
+        console.log("Failed to updated the notification for data.");
+        res.status(500).json({
+          success: false,
+          message: "wish boarad 서버 에러",
+        });
+      } else {
+        console.log("Successfully updated data into the notification!!");
+        res.status(200).json({
+          success: true,
+          message: "알림 수정 성공",
+        });
+      }
+    }
+    db.releaseConn();
+  });
+});
 module.exports = router;
+
